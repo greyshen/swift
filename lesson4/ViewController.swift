69e03b9f32c5dc87ca5d01a8846ec449ba34bf33
Grey Shen <grey.shen@ef.com>
2017-02-06 13:12:44 +0800

    Hello! This is Grey :)

--- lesson4/ViewController.swift
+++ lesson4/ViewController.swift
@@ -13,11 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+        let myLabel = UILabel(frame: CGRect(origin:CGPoint(x:0, y:0),
+                                            size: CGSize(width: 30, height: 30)))
+        myLabel.text = "hello"
+        view.addSubview(myLabel)
     }
 
 
